add07116550c3bdcd9a4b9a52f0315d3b085db2c
Yuzkkj <kevinruan661@gmail.com>
2025-08-27 10:56:19 -0300

    Primeiro Commit

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,9 @@
 //Este e meu primeiro programa CPE
 //Data: 22/08/2025
 #include <iostream>
+using namespace std;
+
 int main() {
-   std::cout << "Ola mundo\n";
+   cout << "Ola mundo\n";
    return 0;
 }
