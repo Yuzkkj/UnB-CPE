893511b4871b68396644d345bb11ab356bc6d1a0
Yuzkkj <kevinruan661@gmail.com>
2025-09-19 10:27:09 -0300

    Aula 19/09/25

---
+++ palindromo_pra_tras.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string palavra = "";
+    cout << "Digite uma palavra para verificar se e palindromo: ";
+    cin >> palavra;
+
+
+    string backwards = "";
+
+    for (int i = palavra.length() - 1; i >= 0; i--)
+    {
+        backwards = backwards + palavra[i];
+    }
+
+    if (palavra == backwards)
+    {
+        cout << "A palavra " << palavra << " e um palindromo." << endl;
+    }
+    else
+    {
+        cout << "A palavra " << palavra << " nao e um palindromo." << endl;
+    }
+
+    system("pause");
+
+    return 0;
+}
